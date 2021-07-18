e48d0f337ec24bfa6b601faf32ac44f124dbe4bd
hieuvvth2009018 <73455351+hieuvvth2009018@users.noreply.github.com>
2021-07-18 13:03:21 +0700

    update file

--- app/Enums/EventStatus.php
+++ app/Enums/EventStatus.php
@@ -2,14 +2,14 @@
 
 namespace App\Enums;
 
-use BenSampo\Enum\Enum;
+use BenSampo\Enums\Enums;
 
 /**
  * @method static static OptionOne()
  * @method static static OptionTwo()
  * @method static static OptionThree()
  */
-final class EventStatus extends Enum
+final class EventStatus extends Enums
 {
     const POST_PONE =0;
     const IS_HAPPENING =1;
